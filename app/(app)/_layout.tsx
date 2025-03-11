8805666752a1bed5a844c0a89c7833917bb64b55
darshanbajania <darshanbajania1999@gmail.com>
2025-03-11 18:09:13 +0530

    chore: remove boiler plate code

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+import { Stack } from "expo-router";
+
+export default function AppLayout() {
+  //   const { session, isLoading } = useSession();
+
+  // You can keep the splash screen open, or render a loading screen like we do here.
+
+  //   This layout can be deferred because it's not the root layout.
+  //   return <Redirect href="/" />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
