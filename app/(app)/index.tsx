92e58d4f78daa06323fdf387af37044c8eb98204
darshanbajania <darshanbajania1999@gmail.com>
2025-03-11 18:32:44 +0530

    feat: setup ui for home page

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ marginBottom: 100 }}>
+        <Text style={{ textAlign: "center", fontSize: 24 }}>
+          90 North Assignment
+        </Text>
+        <Text style={{ textAlign: "center", fontSize: 18 }}>Chat App</Text>
+      </View>
+      <View
+        style={{
+          justifyContent: "flex-end",
+          width: "90%",
+          marginBottom: 50,
+        }}
+      >
+        <Text style={{ color: "black", marginBottom: 16, textAlign: "center" }}>
+          Please create a user to continue
+        </Text>
+        <Text style={{ fontSize: 16 }}>
+          Username <Text style={{ color: "red" }}>*</Text>
+        </Text>
+        <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={40}
+          // onChangeText={(text) => onChangeText(text)}
+          // value={value}
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderRadius: 4,
+            marginTop: 8,
+            color: "black",
+          }}
+        />
+        <Pressable
+          //   onPress={onPressLearnMore}
+
+          //   color="#841584"
+          style={{
+            backgroundColor: "#e8a548",
+            padding: 10,
+            marginTop: 16,
+            alignItems: "center",
+            borderRadius: 4,
+          }}
+          accessibilityLabel="Learn more about this purple button"
+        >
+          <Text style={{ color: "white", fontWeight: 500, fontSize: 16 }}>
+            {" "}
+            {"Create User"}
+          </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+    backgroundColor: "white",
+  },
+});
